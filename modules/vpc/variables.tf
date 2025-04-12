f0b015e4e7a121f5235a4f4e2a7b9656ccd4bf5e
dansarpong <dansarpong.me@gmail.com>
2025-04-12 15:52:12 +0000

    Add VPC module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Project name used for resource tagging"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of Availability Zones to use"
+  type        = list(string)
+}
